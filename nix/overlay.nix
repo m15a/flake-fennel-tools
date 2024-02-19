8e4eed83080a2b14217858afc8bf61b2ebacaa41
NACAMURA Mitsuhiro <m15@m15a.dev>
2024-02-19 15:32:08 +0900

    rafactor: abstract cross product packaging

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -3,133 +3,61 @@
 final: prev:
 
 let
-  fennelWith = { version, src, lua }:
+  fennelVersions = [
+    "stable"
+    "unstable"
+  ];
+
+  luaVersions = [
+    "luajit"
+    "lua5_1"
+    "lua5_2"
+    "lua5_3"
+    "lua5_4"
+  ];
+
+  fennelLuaVersionMatrix = prev.lib.cartesianProductOfSets {
+    fennelVersion = fennelVersions;
+    luaVersion = luaVersions;
+  };
+
+  buildFennel = { fennelVersion, luaVersion }:
     final.callPackage ./pkgs/fennel {
-      inherit version src lua;
+      version = versions."fennel-${fennelVersion}";
+      src = inputs."fennel-${fennelVersion}";
+      lua = final.${luaVersion};
     };
 
-  faithWith = { version, fennel }:
+  buildFaith = { fennelVersion, luaVersion }:
     final.callPackage ./pkgs/faith {
+      version = versions.faith;
       src = inputs.faith;
-      inherit version fennel;
+      fennel = final."fennel-${fennelVersion}-${luaVersion}";
     };
+
+  buildPackageSet = { pname, builder }:
+    builtins.listToAttrs
+      (map
+        ({ fennelVersion, luaVersion } @ args:
+          {
+            name = "${pname}-${fennelVersion}-${luaVersion}";
+            value = builder args;
+          })
+        fennelLuaVersionMatrix);
 in
 
-{
-  fennel = {
-    stable = {
-      luajit = fennelWith {
-        version = versions.fennel-stable;
-        src = inputs.fennel-stable;
-        lua = final.luajit;
-      };
-      lua5_1 = fennelWith {
-        version = versions.fennel-stable;
-        src = inputs.fennel-stable;
-        lua = final.lua5_1;
-      };
-
-      lua5_2 = fennelWith {
-        version = versions.fennel-stable;
-        src = inputs.fennel-stable;
-        lua = final.lua5_2;
-      };
-
-      lua5_3 = fennelWith {
-        version = versions.fennel-stable;
-        src = inputs.fennel-stable;
-        lua = final.lua5_3;
-      };
-
-      lua5_4 = fennelWith {
-        version = versions.fennel-stable;
-        src = inputs.fennel-stable;
-        lua = final.lua5_4;
-      };
-
-    };
-    unstable = {
-      luajit = fennelWith {
-        version = versions.fennel-unstable;
-        src = inputs.fennel-unstable;
-        lua = final.luajit;
-      };
-      lua5_1 = fennelWith {
-        version = versions.fennel-unstable;
-        src = inputs.fennel-unstable;
-        lua = final.lua5_1;
-      };
-      lua5_2 = fennelWith {
-        version = versions.fennel-unstable;
-        src = inputs.fennel-unstable;
-        lua = final.lua5_2;
-      };
-      lua5_3 = fennelWith {
-        version = versions.fennel-unstable;
-        src = inputs.fennel-unstable;
-        lua = final.lua5_3;
-      };
-      lua5_4 = fennelWith {
-        version = versions.fennel-unstable;
-        src = inputs.fennel-unstable;
-        lua = final.lua5_4;
-      };
-    };
-  };
-
-  faith = {
-    stable = {
-      luajit = faithWith {
-        version = versions.faith;
-        fennel = final.fennel.stable.luajit;
-      };
-      lua5_1 = faithWith {
-        version = versions.faith;
-        fennel = final.fennel.stable.lua5_1;
-      };
-      lua5_2 = faithWith {
-        version = versions.faith;
-        fennel = final.fennel.stable.lua5_2;
-      };
-      lua5_3 = faithWith {
-        version = versions.faith;
-        fennel = final.fennel.stable.lua5_3;
-      };
-      lua5_4 = faithWith {
-        version = versions.faith;
-        fennel = final.fennel.stable.lua5_4;
-      };
-    };
-    unstable = {
-      luajit = faithWith {
-        version = versions.faith;
-        fennel = final.fennel.unstable.luajit;
-      };
-      lua5_1 = faithWith {
-        version = versions.faith;
-        fennel = final.fennel.unstable.lua5_1;
-      };
-      lua5_2 = faithWith {
-        version = versions.faith;
-        fennel = final.fennel.unstable.lua5_2;
-      };
-      lua5_3 = faithWith {
-        version = versions.faith;
-        fennel = final.fennel.unstable.lua5_3;
-      };
-      lua5_4 = faithWith {
-        version = versions.faith;
-        fennel = final.fennel.unstable.lua5_4;
-      };
-    };
-  };
-
+(buildPackageSet {
+  pname = "fennel";
+  builder = buildFennel;
+}) // (buildPackageSet {
+  pname = "faith";
+  builder = buildFaith;
+}) // {
   fnlfmt = final.callPackage ./pkgs/fnlfmt {
     version = versions.fnlfmt;
     src = inputs.fnlfmt;
     lua = final.luajit;
   };
-
   fenneldoc = final.callPackage ./pkgs/fenneldoc {
     version = versions.fenneldoc;
     src = inputs.fenneldoc;

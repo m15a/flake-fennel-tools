4e07c38f47313ced7e0a749f665baf8deac6fb8c
NACAMURA Mitsuhiro <m15@m15a.dev>
2024-03-31 09:18:46 +0900

    tools: improve logging in bump-version.bash

--- tools/bump-version.bash
+++ tools/bump-version.bash
@@ -4,15 +4,20 @@ set -euo pipefail
 
 file=CHANGELOG.md
 
+old="$(cat "$file" | grep -Em1 '^## \[[[:digit:]]+\.' | sed -E '1s|.*\[([^]]+)\].*|\1|')"
 bump "$file" "$@"
 git add "$file"
+new="$(cat "$file" | grep -Em1 '^## \[[[:digit:]]+\.' | sed -E '1s|.*\[([^]]+)\].*|\1|')"
 
-version="$(cat "$file" | grep -Em1 '^## \[[[:digit:]]+\.' | sed -E '1s|.*\[([^]]+)\].*|\1|')"
+echo >&2 "Bump version: $old -> $new"
 
-echo $version
+run() {
+    echo >&2 "Run: $@"
+    "$@"
+}
 
 if (( $(git status --short | wc -l) > 0 ))
 then
-    git commit -m "release: $version"
-    git tag "v$version"
+    run git commit -m "release: $new"
+    run git tag "v$new"
 fi

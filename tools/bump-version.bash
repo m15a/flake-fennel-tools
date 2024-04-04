6d801fc16e8e233f4ba49ec94b20f57fecdf7807
NACAMURA Mitsuhiro <m15@m15a.dev>
2024-04-04 11:46:20 +0900

    tools: refactor version bumper

--- tools/bump-version.bash
+++ tools/bump-version.bash
@@ -4,20 +4,24 @@ set -euo pipefail
 
 file=CHANGELOG.md
 
-old="$(cat "$file" | grep -Em1 '^## \[[[:digit:]]+\.' | sed -E '1s|.*\[([^]]+)\].*|\1|')"
-nix run sourcehut:~m15a/bump.fnl -- "$file" "$@"
-git add "$file"
-new="$(cat "$file" | grep -Em1 '^## \[[[:digit:]]+\.' | sed -E '1s|.*\[([^]]+)\].*|\1|')"
-
-echo >&2 "Bump version: $old -> $new"
-
 run() {
-    echo >&2 "Run: $@"
+    echo >&2 "$*"
     "$@"
 }
 
+extract_version_from_changelog() {
+    grep -Em1 '^## \[[[:digit:]]+\.' "$file" \
+        | sed -E '1s|.*\[([^]]+)\].*|\1|'
+}
+
+old="$(extract_version_from_changelog)"
+run nix run sourcehut:~m15a/bump.fnl -- "$file" "$@"
+new="$(extract_version_from_changelog)"
+
 if (( $(git status --short | wc -l) > 0 ))
 then
+    echo >&2 "Bump version: $old -> $new"
+    run git add "$file"
     run git commit -m "release: $new"
     run git tag "v$new"
 fi

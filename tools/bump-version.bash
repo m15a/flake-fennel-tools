0b1d765b687103daefee92fb919620802f59c23f
NACAMURA Mitsuhiro <m15@m15a.dev>
2024-04-10 15:39:22 +0900

    release(flakehub): change to rolling release
    Closes #17

--- tools/bump-version.bash
+++ tools/bump-version.bash
@@ -2,7 +2,8 @@
 
 set -euo pipefail
 
-file=CHANGELOG.md
+changelog=CHANGELOG.md
+flakehub_workflow=.github/workflows/flakehub-publish-rolling.yml
 
 run() {
     echo >&2 "$*"
@@ -10,18 +11,23 @@ run() {
 }
 
 extract_version_from_changelog() {
-    grep -Em1 '^## \[[[:digit:]]+\.' "$file" \
+    grep -Em1 '^## \[[[:digit:]]+\.' "$changelog" \
         | sed -E '1s|.*\[([^]]+)\].*|\1|'
 }
 
 old="$(extract_version_from_changelog)"
-run nix run sourcehut:~m15a/bump.fnl -- "$file" "$@"
+run nix run sourcehut:~m15a/bump.fnl -- --minor "$changelog"
 new="$(extract_version_from_changelog)"
+
+new_minor="$(echo $new | cut -d. -f2)"
+
+sed -Ei "$flakehub_workflow" \
+    -e "s|(rolling-minor: )[[:digit:]]+|\1$new_minor|"
 
 if (( $(git status --short | wc -l) > 0 ))
 then
     echo >&2 "Bump version: $old -> $new"
-    run git add "$file"
+    run git add "$changelog" "$flakehub_workflow"
     run git commit -m "release: $new"
     run git tag "v$new"
 fi
